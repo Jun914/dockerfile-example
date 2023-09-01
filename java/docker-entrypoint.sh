358772479e55efe88e66a0192a67b9f7d83b19d9
김민준 <136772223+Jun914@users.noreply.github.com>
2023-09-01 16:48:17 +0900

    Update docker-entrypoint.sh

--- java/docker-entrypoint.sh
+++ java/docker-entrypoint.sh
@@ -6,7 +6,6 @@ PASSWORD=${PASSWORD:-user}
 useradd -s /bin/bash -m $USERNAME
 echo "$USERNAME:$PASSWORD" | chpasswd
 
-su $USERNAME
-git clone ${GIT_URL}
+su $USERNAME -c "cd ~/ && git clone ${GIT_URL}"
 
 /usr/sbin/sshd -D -e
